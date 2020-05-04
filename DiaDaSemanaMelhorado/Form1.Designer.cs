2abdec6cb95a2a6e049aca7b1039ea1fe75563a9
Dante Souza e Souza <lokinmodar@gmail.com>
2020-05-04 18:35:28 -0300

    Full Commit

--- DiaDaSemanaMelhorado/Form1.Designer.cs
+++ DiaDaSemanaMelhorado/Form1.Designer.cs
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDiaSemana = new System.Windows.Forms.Label();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.cmbDia = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Selecione o número do dia:";
+            // 
+            // lblDiaSemana
+            // 
+            this.lblDiaSemana.AutoSize = true;
+            this.lblDiaSemana.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiaSemana.Location = new System.Drawing.Point(31, 114);
+            this.lblDiaSemana.Name = "lblDiaSemana";
+            this.lblDiaSemana.Size = new System.Drawing.Size(0, 31);
+            this.lblDiaSemana.TabIndex = 2;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(25, 69);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+            this.btnVerificar.TabIndex = 3;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(178, 69);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 4;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // cmbDia
+            // 
+            this.cmbDia.FormattingEnabled = true;
+            this.cmbDia.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.cmbDia.Location = new System.Drawing.Point(178, 33);
+            this.cmbDia.Name = "cmbDia";
+            this.cmbDia.Size = new System.Drawing.Size(76, 21);
+            this.cmbDia.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 180);
+            this.ControlBox = false;
+            this.Controls.Add(this.cmbDia);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.lblDiaSemana);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Dia da Semana";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDiaSemana;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.ComboBox cmbDia;
     }
 }
 
--- DiaDaSemanaMelhorado/Form1.cs
+++ DiaDaSemanaMelhorado/Form1.cs
@@ -16,5 +16,51 @@ namespace DiaDaSemanaMelhorado
         {
             InitializeComponent();
         }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            lblDiaSemana.ResetText();
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblDiaSemana.ResetText();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            int dia;
+            dia = Convert.ToInt32(cmbDia.Text);
+            Console.WriteLine(dia);
+
+            switch (dia)
+            {
+                case 1:
+                    lblDiaSemana.Text = "Domingo";
+                    break;
+                case 2:
+                    lblDiaSemana.Text = "Segunda-feira";
+                    break;
+                case 3:
+                    lblDiaSemana.Text = "Terça-feira";
+                    break;
+                case 4:
+                    lblDiaSemana.Text = "Quarta-feira";
+                    break;
+                case 5:
+                    lblDiaSemana.Text = "Quinta-feira";
+                    break;
+                case 6:
+                    lblDiaSemana.Text = "Sexta-feira";
+                    break;
+                case 7:
+                    lblDiaSemana.Text = "Sábado";
+                    break;
+                default:
+                    lblDiaSemana.Text = "Dia inválido";
+                    break;
+            }
+        }
     }
 }
